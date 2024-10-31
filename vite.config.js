6559d1753ba8dae092957b05be28c011e762ba88
Ishfaq-dev <contact.muhammadishfaq@gmail.com>
2024-10-31 20:34:24 +0500

    intial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Ecommerce-app/',  // Add this line with the repository name
+})
